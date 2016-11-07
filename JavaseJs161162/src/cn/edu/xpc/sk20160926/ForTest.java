cb4cf0b78c58addc2f541cedf7ebcfda93fe1878
HongyanLi <admin@5uu.net>
2016-11-07 10:39:43 +0800

    ll

--- JavaseJs161162/src/cn/edu/xpc/sk20160926/ForTest.java
+++ JavaseJs161162/src/cn/edu/xpc/sk20160926/ForTest.java
@@ -1,12 +1,29 @@
 package cn.edu.xpc.sk20160926;
 
+import java.util.Scanner;
+
 public class ForTest {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 100; i++) {
-			System.out.println("这是每"+(i+1)+"遍");
+		/*
+		 * for (int i = 0; i < 100; i++) { System.out.println("这是每"+(i+1)+"遍");
+		 * }
+		 */
+
+		int score=0;
+		Scanner input=new Scanner(System.in);
+		int sum=0;
+		String name="张三";
+		for (int i = 0; i < 5; i++) { // 循环5次录入5门课成绩
+			System.out.print("请输入5门功课中第" + (i + 1) + "门课的成绩： ");
+			score = input.nextInt();
+			sum = sum + score;
 		}
+
+		double avg = sum / 5; // 计算平均分
+		System.out.println(name + "的平均分是：" + avg);
+
 	}
 
 }
